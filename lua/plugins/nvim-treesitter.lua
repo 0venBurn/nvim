097acedfc00e1ca17e315e6cc1ff45c3c2ed54eb
Evan Byrne <evanbyrnecode@gmail.com>
2025-11-11 12:25:37 +0000

    updated config

--- lua/plugins/nvim-treesitter.lua
+++ lua/plugins/nvim-treesitter.lua
@@ -2,25 +2,58 @@
 return {
 	-- https://github.com/nvim-treesitter/nvim-treesitter
 	"nvim-treesitter/nvim-treesitter",
-	event = "VeryLazy",
+	branch = "master", -- Important: specify master branch as main will be default in future
+	lazy = false, -- treesitter does not support lazy-loading
 	dependencies = {
 		-- https://github.com/nvim-treesitter/nvim-treesitter-textobjects
 		"nvim-treesitter/nvim-treesitter-textobjects",
 	},
 	build = ":TSUpdate",
 	opts = {
-		highlight = {
-			enable = true,
-		},
-		indent = { enable = false },
-		auto_install = true, -- automatically install syntax support when entering new file type buffer
+		-- A list of parser names, or "all" (the listed parsers MUST always be installed)
 		ensure_installed = {
+			"c",
 			"lua",
+			"vim",
+			"vimdoc",
+			"query",
+			"markdown",
+			"markdown_inline",
 			"comment",
 			"xml",
 			"http",
 			"json",
 			"graphql",
+		},
+
+		-- Install parsers synchronously (only applied to `ensure_installed`)
+		sync_install = false,
+
+		-- Automatically install missing parsers when entering buffer
+		auto_install = true,
+
+		-- List of parsers to ignore installing (or "all")
+		ignore_install = {},
+
+		highlight = {
+			enable = true,
+			-- disable = { "python" }, -- Removed: consider re-enabling for all languages
+			-- Set this to `true` if you depend on 'syntax' being enabled (like for indentation).
+			-- Using this option may slow down your editor, and you may see some duplicate highlights.
+			additional_vim_regex_highlighting = false,
+		},
+
+		indent = { enable = false }, -- Experimental feature
+
+		-- Incremental selection based on grammar
+		incremental_selection = {
+			enable = true,
+			keymaps = {
+				init_selection = "gnn",
+				node_incremental = "grn",
+				scope_incremental = "grc",
+				node_decremental = "grm",
+			},
 		},
 	},
 	config = function(_, opts)
